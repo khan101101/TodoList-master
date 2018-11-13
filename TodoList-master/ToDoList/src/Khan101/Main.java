70508b6b3eac265fc058dbb634ead681fa6c5544
khan101101 <38144545+khan101101@users.noreply.github.com>
2018-11-13 15:11:12 +0100

    TodoItems saved in a text file - simple Menu added

--- TodoList-master/ToDoList/src/Khan101/Main.java
+++ TodoList-master/ToDoList/src/Khan101/Main.java
@@ -1,10 +1,13 @@
 package Khan101;
 
+import Khan101.datamodel.TodoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -14,10 +17,31 @@ public class Main extends Application {
         primaryStage.setTitle("Todo List");
         primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception{
+        try{
+            TodoData.getInstance().storeTodoItems();
+
+        } catch (IOException e){
+            System.out.println((e.getMessage()));
+        }
+    }
+
+    @Override
+    public void init() throws Exception{
+        try{
+            TodoData.getInstance().loadTodoItems();
+
+        } catch (IOException e){
+            System.out.println((e.getMessage()));
+        }
     }
 }
--- TodoList-master/ToDoList/src/Khan101/datamodel/TodoData.java
+++ TodoList-master/ToDoList/src/Khan101/datamodel/TodoData.java
@@ -37,9 +37,9 @@ public class TodoData {
         return todoItems;
     }
 
-    public void setTodoItems(List<TodoItem> todoItems) {
-        this.todoItems = todoItems;
-    }
+   // public void setTodoItems(List<TodoItem> todoItems) {
+    //    this.todoItems = todoItems;
+   // }
 
     public void loadTodoItems() throws IOException {
 
